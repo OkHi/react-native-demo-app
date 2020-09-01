dbb2db643c7bee3607ff10b3a5934cb76eff0514
Julius Kiano <jgkiano@gmail.com>
2020-09-01 16:10:07 +0300

    added react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {LoaderScreen} from './screens/Loader';
+import {CreateAddressScreen} from './screens/CreateAddress';
+
+const Stack = createStackNavigator();
 
 export const App = () => {
-  return <LoaderScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoaderScreen"
+          component={LoaderScreen}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="CreateAddressScreen"
+          component={CreateAddressScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
