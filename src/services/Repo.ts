a8e395a15d71b049dadc35d5cfb513da91301a15
Julius Kiano <jgkiano@gmail.com>
2020-09-01 17:04:55 +0300

    provision for get and save user

--- src/services/Repo.ts
+++ src/services/Repo.ts
@@ -1,5 +1,27 @@
-export class Repo {
-  static getUser() {
-    return null;
+import AsyncStorage from '@react-native-community/async-storage';
+
+type User = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  id?: string;
+};
+
+export default class Repo {
+  static async getUser(): Promise<User | null> {
+    try {
+      const user = await AsyncStorage.getItem('user');
+      return user !== null ? JSON.parse(user) : null;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  static async saveUser(user: User): Promise<void> {
+    return AsyncStorage.setItem('user', JSON.stringify(user));
+  }
+
+  static async wipe() {
+    return AsyncStorage.clear();
   }
 }
