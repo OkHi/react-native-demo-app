adb31eedb1d7d88cb289a623ad87a9659c138703
Julius Kiano <jgkiano@gmail.com>
2020-09-01 12:36:18 +0300

    show LoaderScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {LoaderScreen} from './screens/Loader';
 
 export const App = () => {
-  return (
-    <View>
-      <Text>Hi</Text>
-    </View>
-  );
+  return <LoaderScreen />;
 };
