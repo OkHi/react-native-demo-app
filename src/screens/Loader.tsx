d758c14784a32c738e36b359febe3f7fed4623d5
Julius Kiano <jgkiano@gmail.com>
2020-09-01 17:05:14 +0300

    loader to check user and navigate

--- src/screens/Loader.tsx
+++ src/screens/Loader.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ActivityIndicator} from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {CommonActions} from '@react-navigation/native';
 import styled from 'styled-components/native';
 import {Container as Parent} from '../components/Common';
+import Repo from '../services/Repo';
+import {RootStackParamList} from '../routes';
 
 const values = [
   'Be open',
@@ -11,7 +15,34 @@ const values = [
   'Be bold',
 ];
 
-export const LoaderScreen = () => {
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'LoaderScreen'>;
+};
+
+export const LoaderScreen = (props: Props) => {
+  const navigate = (route: string) => {
+    const reset = CommonActions.reset({
+      index: 0,
+      routes: [{name: route}],
+    });
+    props.navigation.dispatch(reset);
+  };
+
+  const init = () => {
+    const timer = setTimeout(() => {
+      Repo.getUser()
+        .then((user) => {
+          if (!user) {
+            navigate('CreateAddressScreen');
+          }
+        })
+        .catch(() => navigate('CreateAddressScreen'));
+      clearTimeout(timer);
+    }, 1000);
+  };
+
+  useEffect(init, []);
+
   const getValue = () => {
     const random = Math.floor(Math.random() * values.length);
     return values[random];
