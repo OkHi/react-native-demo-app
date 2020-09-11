c86349eeb49b58726d17914a215804dceee80551
Julius Kiano <jgkiano@gmail.com>
2020-09-11 09:00:13 +0300

    address support

--- src/services/Repo.ts
+++ src/services/Repo.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import {User} from '../types';
+import {User, Address} from '../types';
 
 export default class Repo {
   static async getUser(): Promise<User | null> {
@@ -17,5 +17,41 @@ export default class Repo {
 
   static async wipe() {
     return AsyncStorage.clear();
+  }
+
+  static async saveAddress(address: Address) {
+    const addresses = await this.getAllAddresses();
+    addresses.unshift(address);
+    return this.saveAddresses(addresses);
+  }
+
+  static async patchAddress<T>(addressId: string, update: T) {
+    let addresses = await this.getAllAddresses();
+    addresses = addresses.map((addr) => {
+      if (addr.id === addressId) {
+        return {...addr, ...update};
+      }
+      return addr;
+    });
+    return this.saveAddresses(addresses);
+  }
+
+  static async getAllAddresses(): Promise<Array<Address>> {
+    try {
+      const addresses = await AsyncStorage.getItem('addresses');
+      return addresses !== null ? JSON.parse(addresses) : null;
+    } catch (error) {
+      return [];
+    }
+  }
+
+  static async saveAddresses(addresses: Array<Address>) {
+    return AsyncStorage.setItem('addresses', JSON.stringify(addresses));
+  }
+
+  static async removeAddress(addressId: string) {
+    let addresses = await this.getAllAddresses();
+    addresses = addresses.filter((address) => address.id !== addressId);
+    return this.saveAddresses(addresses);
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,10 @@
+import {OkHiLocation} from '@okhi/react-native-core';
+
 export type User = {
   firstName: string;
   lastName: string;
   phone: string;
   id?: string;
 };
+
+export type Address = OkHiLocation & {verifying: boolean};
